4d5aba232413fd595e9029064701277b14b418e5
sndtkrh <sndtkrh@gmail.com>
2018-02-25 02:24:08 +0900

    add comments

--- include/qalgorithm.hpp
+++ include/qalgorithm.hpp
@@ -24,7 +24,11 @@ void deutsch_jozsa(Qcircuit & qc, std::function<bool(std::size_t)> f){
   }
 }
 
-// apply grover rotation to qc's head_n q-bits from head
+/*
+* apply Grover rotation to qc's head_n q-bits from head.
+* Grover rotation G is equal to (2|ψ><ψ| - I)O,
+* whire O is quantum oracle and |ψ> = 1 / √(2^n) Σ |x>.
+*/
 void grover_rotation(Qcircuit & qc,
     qbitsize head_n,
     std::function<Qstate(Qstate &)> & qoracle){
@@ -58,8 +62,12 @@ void grover_rotation(Qcircuit & qc,
   }
 }
 
-#include <iostream>
-using namespace std;
+/*
+* Grover's search algorithm
+* - O(√N) time where N = 2^n, N is total data size.
+* - Pr(success) = Θ(1)
+* - χ : {0,1,2,...,N-1} → Bool
+*/
 void grover_search(Qcircuit & qc,
     const std::function<bool(std::size_t)> & chi,
     std::size_t sol_n){
