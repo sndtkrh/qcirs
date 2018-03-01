a60509dcdae555ed5fa82e7e9fd751c5d6552412
sndtkrh <sndtkrh@gmail.com>
2018-03-01 23:12:19 +0900

    add file input mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,57 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <exception>
 #include <stdexcept>
 #include "qcparser/parser.hpp"
 #include "qcparser/interpreter.hpp"
 
-void print_logo(){
-  const std::string logo ="\
-  _____    _____   _         ____     \n\
- / ___ \\  /  ___\\ ( )  _  _ (  __\\ \n\
-( (_,_) )(  (____ | | | |'/__\\ \\    \n\
- \\___  /  \\_____/ |_| |_|  \\____)  \n\
-     \\_\\                            \n\
-";
-std::cout << logo << std::flush;
-std::cout << "QCirS -- Quantum Circuit Simulator" << std::endl;
-std::cout << "==================================" << std::endl;
-std::cout << std::endl;
+void interactive_mode();
+void file_mode(const std::string & filename);
+
+int main(int argc, char* argv[]){
+  if( argc == 0 ){
+    interactive_mode();
+  }else if( argc == 1 ){
+    std::string filename = argv[1];
+    file_mode();
+  }else if( argc != 0 ){
+    std::cout << "QCirS" << std::endl;
+    std::cout << "USAGE : " << std::endl;
+    std::cout << "    qcirs <input>      run <input>" << std::endl;
+    std::cout << "    qcirs              interactive mode" << std::endl;
+  }
+  return 0;
 }
 
-int main(){
+void file_mode(const std::string & filename){
+  std::ifstream fin;
+  fin.open(filename, std::ios::in);
+  if( !fin ){
+    std::cout << "error : cannot open file " << filename << "." << std::endl;
+    exit(1);
+  }
+  while(std::getline(fin, source)){
+    qcparser::env e;
+    std::string source;
+    while(std::getline(fin, source)){
+      try{
+        qcparser::program program = qcparser::parse(source);
+        qcparser::interpret(program, e);
+      }catch(const std::string & s){
+        std::cout << s << std::endl;
+        exit(1);
+      }catch(std::exception & e){
+        std::cout << "ERROR : " << e.what() << std::endl;
+        exit(1);
+      }
+    }
+  }
+  fin.close();
+}
+
+void print_logo();
+void interactive_mode(){
   print_logo();
 
   qcparser::env e;
@@ -39,4 +71,18 @@ int main(){
     std::cout << ">> " << std::flush;
   }
   std::cout << std::endl;
+}
+
+void print_logo(){
+  const std::string logo ="\
+  _____    _____   _         ____     \n\
+ / ___ \\  /  ___\\ ( )  _  _ (  __\\ \n\
+( (_,_) )(  (____ | | | |'/__\\ \\    \n\
+ \\___  /  \\_____/ |_| |_|  \\____)  \n\
+     \\_\\                            \n\
+";
+std::cout << logo << std::flush;
+std::cout << "QCirS -- Quantum Circuit Simulator" << std::endl;
+std::cout << "==================================" << std::endl;
+std::cout << std::endl;
 }
